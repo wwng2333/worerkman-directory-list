8318eb6af8d51054259eea9d5a4014e7b680668c
wwng2333 <wwng@2333.me>
2017-03-16 22:03:06 +0800

    first commit

---
+++ demo_server.php
@@ -0,0 +1,117 @@
+﻿<?php
+ini_set('memory_limit','-1');
+use Workerman\Worker;
+use Workerman\Protocols\Http;
+require_once __DIR__ . '/vendor/Autoloader.php';
+
+$GLOBALS['path'] = '/home/csgoserver/serverfiles/csgo/';
+
+function formatsize($size, $key = 0) {
+	if($size < 0) {
+		return '0B';
+	} else {
+		$danwei = array('B','K','M','G','T','P');
+		while($size > 1024) {
+			$size = $size / 1024;
+			$key++;
+		}
+		return round($size, 0).$danwei[$key];
+	}
+}
+
+function get_ver($data) {
+	$_d = $data['server'];
+	$time_usage = round((microtime(true) - $GLOBALS['time_start']) * 1000, 4);
+	$mem_usage = round(memory_get_usage()/1024/1024, 2);
+	$_s = "Processed in {$time_usage} ms , {$mem_usage} MB memory used , {$GLOBALS['queries']} queries.</br>\n";
+	return sprintf($_s.'%s Server at %s Port %s', $_d['SERVER_SOFTWARE'], $_d['SERVER_NAME'], $_d['SERVER_PORT']);
+}
+
+function get_create_time($filename) {
+	$temp = explode('_', $filename);
+	if(isset($temp[0]) and strlen($temp[0]) == 14) return strtotime($temp[0]);
+}
+function read_dir($dir, $sort = 'mtime', $order = SORT_DESC) {
+	$handler = opendir($dir);
+	while($filename = readdir($handler)) {
+		if(strstr($filename, '.dem')) {
+			$file_name[] = $filename;
+			$file_path[] = $real_path = $GLOBALS['path'].$filename;
+			$file_size[] = filesize($real_path);
+			$file_ctime[] = $start_time = get_create_time($filename);
+			$file_mtime[] = $end_time = filemtime($real_path);
+			$duration[] = $end_time - $start_time;
+			$GLOBALS['queries'] += 2;
+		}
+	}
+	closedir($handler);
+	switch($sort) {
+		case 'name':
+			array_multisort($file_name, $order, $file_size, $file_mtime, $duration);
+		break;
+		case 'size':
+			array_multisort($file_size, $order, $file_name, $file_mtime, $duration);
+		break;
+		case 'duration':
+			array_multisort($duration, $order, $file_size, $file_name, $file_mtime);
+		break;
+		case 'mtime':
+			array_multisort($file_mtime, $order, $file_size, $file_name, $duration);
+		break;
+		default: break;
+	}
+	return (isset($file_name)) ? array('name' => $file_name, 'size' => $file_size, 'mtime' => $file_mtime, 'duration' => $duration) : false;
+}
+
+function make_list($array) {
+	#var_dump($array);
+	if(!$array) return false;
+	$str = '';
+	for($i=0;$i<count($array['name']);$i++) {
+		$file_now = $array['name'][$i];
+		$size_now = formatsize($array['size'][$i]);
+		$mtime_now = date("Y-m-d H:i", $array['mtime'][$i]);
+		$duration = floor($array['duration'][$i] / 60);
+		$str .= "<tr>\n";
+		$str .= "<td><a href=\"?download=$file_now\"> $file_now</a></td>\n";
+		$str .= "<td align=\"right\"> $mtime_now</td>\n";
+		$str .= "<td align=\"right\"> $duration 分钟</td>\n";
+		$str .= "<td align=\"right\"> $size_now</td><td>&nbsp;</td>\n";
+		$str .= "<td align=\"right\"><a href=\"?delete=$file_now\" onclick=\"return confirm('确定要删除吗？')\"> 删除</a></td>\n";
+		$str .= "</tr>\n";
+	}
+	return $str;
+}
+
+function get_full_html($table, $data) {
+	$header = "<!DOCTYPE html PUBLIC \"-//WAPFORUM//DTD XHTML Mobile 1.0//EN\" \"http://www.wapforum.org/DTD/xhtml-mobile10.dtd\">\n<html xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n<title>Index of /</title>\n<style type=\"text/css\" media=\"screen\">pre{background:0 0}body{margin:2em}</style>\n<script>if(window.name!=\"bencalie\"){location.reload();window.name=\"bencalie\"}else{window.name=\"\"}</script>\n</head>\n<body>\n<strong>Demo 下载</strong>\n";
+	$footer = "<address>%s</address>\n</body>\n</html>";
+	$template_a = $header.'<p>没有文件</p>'.$footer;
+	$template = $header.'<table><tr><th><a>名称</a></th><th><a>最后更改</a></th><th><a>持续时间</a></th><th><a>大小</a></th></tr><tr><th colspan="5"><hr></th></tr>%s<tr><th colspan="5"><hr></th></tr></table>'.$footer;
+	if(!$table) return sprintf($template_a, get_ver($data));
+	return sprintf($template, $table, get_ver($data));
+}
+
+$http_worker = new Worker("http://0.0.0.0:12101");
+$http_worker->count = 4;
+$http_worker->onMessage = function($connection, $data) {
+	$GLOBALS['time_start'] = microtime(true);
+	$GLOBALS['queries'] = 0;
+	if(isset($_GET['download'])) {
+		$file_path = $GLOBALS['path'].$_GET['download'];
+		// Use Workerman\Protocols\Http::header to send header to clients.
+		Http::header("Content-type: text/plain");
+		Http::header("Accept-Ranges: bytes");
+		Http::header("Content-Disposition: attachment; filename=".$_GET['download']);
+		$connection->send(file_get_contents($file_path));
+	} elseif(isset($_GET['delete'])) {
+		unlink($GLOBALS['path'].$_GET['delete']);
+		$connection->send('<script>history.go(-1)</script>');
+	} elseif(isset($_GET['sort'])) {
+		$connection->send(get_full_html(make_list(read_dir($GLOBALS['path'], $_GET['sort'])), $data));
+	} else {
+		$connection->send(get_full_html(make_list(read_dir($GLOBALS['path'])), $data));
+	}
+};
+
+Worker::runAll();
