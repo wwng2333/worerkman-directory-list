a9f22761f6d818e917fda1d7186ad23dc70d7d3a
wwng2333 <wwng@2333.me>
2017-04-02 23:50:32 +0800

    bug image

--- start.php
+++ start.php
@@ -54,7 +54,7 @@ function read_dir($dir, $sort = 'name', $order = SORT_DESC) {
 	return (isset($file_name)) ? array('name' => $file_name, 'size' => $file_size, 'mtime' => $file_mtime, 'dir' => $is_dir) : false;
 }
 
-function make_list($dir, $array) {
+function make_list($dir, $array) { 
 	if(!$array) return false;
 	$str = '';
 	$GLOBALS['total_files'] = 0;
@@ -65,7 +65,7 @@ function make_list($dir, $array) {
 		if($array['dir'][$i]) {
 			$mtime_now = date("Y-m-d H:i", $array['mtime'][$i]);
 			$str .= "<tr>\n";
-			$str .= "<td><img src=\"data:image/gif;base64,R0lGODlhFAAWAMIAAP/////Mmcz//5lmMzMzMwAAAAAAAAAAACH+TlRoaXMgYXJ0IGlzIGluIHRoZSBwdWJsaWMgZG9tYWluLiBLZXZpbiBIdWdoZXMsIGtldmluaEBlaXQuY29tLCBTZXB0ZW1iZXIgMTk5NQAh+QQBAAACACwAAAAAFAAWAAADVCi63P4wyklZufjOErrvRcR9ZKYpxUB6aokGQyzHKxyO9RoTV54PPJyPBewNSUXhcWc8soJOIjTaSVJhVphWxd3CeILUbDwmgMPmtHrNIyxM8Iw7AQA7\" alt=\"[DIR]\"></td><td> <a href=\"?dir=$real_path\">$name</a></td>\n";
+			$str .= "<td><img src=\"?gif=dir\" alt=\"[DIR]\"></td><td> <a href=\"?dir=$real_path\">$name</a></td>\n";
 			$str .= "<td align=\"right\"> $mtime_now</td>\n";
 			$str .= "<td align=\"right\"> -</td><td>&nbsp;</td>\n";
 			$str .= "<td align=\"right\"><a href=\"?delete=$name\" onclick=\"return confirm('确定要删除吗？')\"> 删除</a></td>\n";
@@ -75,7 +75,7 @@ function make_list($dir, $array) {
 			$size_now = formatsize($array['size'][$i]);
 			$mtime_now = date("Y-m-d H:i", $array['mtime'][$i]);
 			$str .= "<tr>\n";
-			$str .= "<td><img src=\"data:image/gif;base64,R0lGODlhFAAWAMIAAP///8z//8zMzJmZmTMzMwAAAAAAAAAAACH+TlRoaXMgYXJ0IGlzIGluIHRoZSBwdWJsaWMgZG9tYWluLiBLZXZpbiBIdWdoZXMsIGtldmluaEBlaXQuY29tLCBTZXB0ZW1iZXIgMTk5NQAh+QQBAAABACwAAAAAFAAWAAADaUi6vPEwEECrnSS+WQoQXSEAE6lxXgeopQmha+q1rhTfakHo/HaDnVFo6LMYKYPkoOADim4VJdOWkx2XvirUgqVaVcbuxCn0hKe04znrIV/ROOvaG3+z63OYO6/uiwlKgYJJOxFDh4hTCQA7\" alt=\"[   ]\"></td><td> <a href=\"?download=$real_path\">$name</a></td>\n";
+			$str .= "<td><img src=\"?gif=blank\" alt=\"[   ]\"></td><td> <a href=\"?download=$real_path\">$name</a></td>\n";
 			$str .= "<td align=\"right\"> $mtime_now</td>\n";
 			$str .= "<td align=\"right\"> $size_now</td><td>&nbsp;</td>\n";
 			$str .= "<td align=\"right\"><a href=\"?delete=$name\" onclick=\"return confirm('确定要删除吗？')\"> 删除</a></td>\n";
@@ -95,7 +95,7 @@ function get_full_html($path, $sort, $data) {
 	$header = "<!DOCTYPE html PUBLIC \"-//WAPFORUM//DTD XHTML Mobile 1.0//EN\" \"http://www.wapforum.org/DTD/xhtml-mobile10.dtd\">\n<html xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n<title>Index of /</title>\n<style type=\"text/css\" media=\"screen\">pre{background:0 0}body{margin:2em}tb{width:600px;margin:0 auto}</style>\n<script>if(window.name!=\"bencalie\"){location.reload();window.name=\"bencalie\"}else{window.name=\"\"}</script>\n</head>\n<body>\n<strong>$path 的索引</strong>\n";
 	$footer = "<address>%s</address>\n</body>\n</html>";
 	$template_a = $header.'<p>没有文件</p>'.$footer;
-	$template = $header.'<table><th><img src="data:image/gif;base64,R0lGODlhFAAWAKEAAP///8z//wAAAAAAACH+TlRoaXMgYXJ0IGlzIGluIHRoZSBwdWJsaWMgZG9tYWluLiBLZXZpbiBIdWdoZXMsIGtldmluaEBlaXQuY29tLCBTZXB0ZW1iZXIgMTk5NQAh+QQBAAABACwAAAAAFAAWAAACE4yPqcvtD6OctNqLs968+w+GSQEAOw==" alt="[ICO]"></th><th><a href="?sort=name">名称</a></th><th><a href="?sort=mtime">最后更改</a></th><th><a href="?sort=size">大小</a></th></tr><tr><th colspan="6"><hr></th></tr>%s<tr><th colspan="6"><hr></th></tr></table>'.$footer;
+	$template = $header.'<table><th><img src="?gif=ico" alt="[ICO]"></th><th><a href="?sort=name">名称</a></th><th><a href="?sort=mtime">最后更改</a></th><th><a href="?sort=size">大小</a></th></tr><tr><th colspan="6"><hr></th></tr>%s<tr><th colspan="6"><hr></th></tr></table>'.$footer;
 	if(!$table) return sprintf($template_a, get_ver($data));
 	return sprintf($template, $table, "{$GLOBALS['total_files']} files, total {$GLOBALS['total_size']}.</br>".get_ver($data));
 }
@@ -105,6 +105,21 @@ $http_worker->count = 4;
 $http_worker->onMessage = function($connection, $data) {
 	$GLOBALS['time_start'] = microtime(true);
 	$GLOBALS['queries'] = 0;
+	if(isset($_GET['gif'])) {
+		switch($_GET['gif']) {
+			case 'dir' :
+				$gif = 'R0lGODlhFAAWAMIAAP/////Mmcz//5lmMzMzMwAAAAAAAAAAACH+TlRoaXMgYXJ0IGlzIGluIHRoZSBwdWJsaWMgZG9tYWluLiBLZXZpbiBIdWdoZXMsIGtldmluaEBlaXQuY29tLCBTZXB0ZW1iZXIgMTk5NQAh+QQBAAACACwAAAAAFAAWAAADVCi63P4wyklZufjOErrvRcR9ZKYpxUB6aokGQyzHKxyO9RoTV54PPJyPBewNSUXhcWc8soJOIjTaSVJhVphWxd3CeILUbDwmgMPmtHrNIyxM8Iw7AQA7';
+			break;
+			case 'ico' :
+				$gif = 'R0lGODlhFAAWAKEAAP///8z//wAAAAAAACH+TlRoaXMgYXJ0IGlzIGluIHRoZSBwdWJsaWMgZG9tYWluLiBLZXZpbiBIdWdoZXMsIGtldmluaEBlaXQuY29tLCBTZXB0ZW1iZXIgMTk5NQAh+QQBAAABACwAAAAAFAAWAAACE4yPqcvtD6OctNqLs968+w+GSQEAOw==';
+			break;
+			case 'blank' :
+				$gif = 'R0lGODlhFAAWAMIAAP///8z//8zMzJmZmTMzMwAAAAAAAAAAACH+TlRoaXMgYXJ0IGlzIGluIHRoZSBwdWJsaWMgZG9tYWluLiBLZXZpbiBIdWdoZXMsIGtldmluaEBlaXQuY29tLCBTZXB0ZW1iZXIgMTk5NQAh+QQBAAABACwAAAAAFAAWAAADaUi6vPEwEECrnSS+WQoQXSEAE6lxXgeopQmha+q1rhTfakHo/HaDnVFo6LMYKYPkoOADim4VJdOWkx2XvirUgqVaVcbuxCn0hKe04znrIV/ROOvaG3+z63OYO6/uiwlKgYJJOxFDh4hTCQA7';
+			break;
+		}
+		Http::header("Content-type: image/gif");
+		$connection->send(base64_decode($gif));
+	}
 	if(!isset($_GET['sort'])) $_GET['sort'] = false;
 	if(!isset($_GET['dir'])) {
 		$_GET['dir'] = '';
